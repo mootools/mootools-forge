37ba7ccf0885a23e90e5256051c52b6d05aa765a
Tim Wienk <tim@wienk.name>
2013-05-30 10:15:39 +0200

    Upgrade Forge to work with Symfony 1.4.20.

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -6,6 +6,6 @@ sfCoreAutoload::register();
 class ProjectConfiguration extends sfProjectConfiguration {
 	public function setup() {
 		// for compatibility / remove and enable only the plugins you want
-		$this->enableAllPluginsExcept(array('sfDoctrinePlugin', 'sfCompat10Plugin'));
+		$this->enableAllPluginsExcept(array('sfDoctrinePlugin'));
 	}
 }

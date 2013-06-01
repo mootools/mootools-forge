7bf1b17868a2f86b917181559b345338e4c2d9f0
Tim Wienk <tim@wienk.name>
2013-06-01 14:12:29 +0200

    Whitespace in model classes.

--- lib/model/PluginTag.php
+++ lib/model/PluginTag.php
@@ -2,28 +2,28 @@
 
 class PluginTag extends BasePluginTag
 {
-	
+
 	public function __toString(){
-    return $this->getName();
+		return $this->getName();
 	}
-	
+
 	public function getName($strict = false){
-	  if ($strict && is_numeric($this->name)) return floatval($this->name);
-	  return $this->name;
+		if ($strict && is_numeric($this->name)) return floatval($this->name);
+		return $this->name;
 	}
-	
+
 	public function getDownloadLink($type = 'zipball'){
 		$plugin = $this->getPlugin();
 		return sprintf("http://github.com/%s/%s/%s/%s", $plugin->getGithubuser(), $plugin->getGithubrepo(), $type, $this->getName());
 	}
-	
+
 	public function sumDownload($save = true){
 		$this->setDownloadsCount($this->getDownloadsCount() + 1);
 		if ($save) $this->save();
 	}
-	
+
 	public function isCurrent(){
 		return $this->getCurrent();
 	}
-	
+
 }
--- lib/model/Term.php
+++ lib/model/Term.php
@@ -2,14 +2,15 @@
 
 class Term extends BaseTerm
 {
-	
+
 	public function __toString(){
 		return $this->getTitle();
 	}
-	
+
 }
 
-$columns_map = array('from'   => TermPeer::TITLE,
-                     'to'     => TermPeer::SLUG);
+$columns_map = array('from' => TermPeer::TITLE, 'to' => TermPeer::SLUG);
 
-sfPropelBehavior::add('Term', array('sfPropelActAsSluggableBehavior' => array('columns' => $columns_map, 'separator' => '_', 'permanent' => true)));
+sfPropelBehavior::add('Term', array(
+	'sfPropelActAsSluggableBehavior' => array('columns'=>$columns_map, 'separator'=>'_', 'permanent'=>true)
+));
